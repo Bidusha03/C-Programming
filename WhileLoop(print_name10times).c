159e5262e8678edd5fb65e8be172a85ccd9ef6ac
Bidusha Halder <149459802+Bidusha03@users.noreply.github.com>
2023-12-16 15:04:16 +0530

    Add files via upload

---
+++ WhileLoop(print_name10times).c
@@ -0,0 +1,30 @@
+//Write a program in C TO PRINT YOUR NAMES 10 TIMES using for loop.
+
+#include <stdio.h>
+int main() {
+    int i = 0; // Initialize the variable i with 0
+
+    // Use a while loop to repeat the printing statement 100 times
+    while (i < 10) {
+        // Print the string "Bidusha"
+        printf("Bidusha\n");
+
+        i = i + 1; // Increment the value of i by 1
+    }
+
+    return 0; 
+}
+
+/*
+Output:
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+Bidusha
+*/
